8f6b92bb7a9dc75b883e56d805069de84a88a81d
Victory <tuduov@gmail.com>
2021-10-18 17:19:47 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,14 +3,7 @@ import "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAsRJUtMkA2SK2gEpXSX-GbJXgP5oyIoAQ",
-  authDomain: "music-application-1d155.firebaseapp.com",
-  databaseURL: "https://music-application-1d155-default-rtdb.firebaseio.com",
-  projectId: "music-application-1d155",
-  storageBucket: "music-application-1d155.appspot.com",
-  messagingSenderId: "665357796075",
-  appId: "1:665357796075:web:f06db248b63683a8f3b4ae",
-  measurementId: "G-WL3BLDDZ9D",
+ //from your dashboard
 };
 
 !firebase.apps.length
